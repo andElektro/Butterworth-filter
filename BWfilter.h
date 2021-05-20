01e9b412a9524b7a5d5167a2a09b158a6d3ef910
andElektro <81776894+andElektro@users.noreply.github.com>
2021-05-20 20:22:34 +0200

    Update BWfilter.h

--- BWfilter.h
+++ BWfilter.h
@@ -7,7 +7,6 @@
    Ex:
    fc = 10Hz //Cut off frequency
    fs = 1kHz //Arduino sampling frequency
-   n = 4 //Order of filter
    Difference equation 4:th order HP Butterwothfilter:
    y[n] = 0.9212x[n] - 3.685x[n-1] + 5.527x[n-2] - 3.685x[n-3] + 0.9212x[n-4] + 3.836y[n-1] - 5.521y[n-2] + 3.534y[n-3] - 0.8486y[n-4]
    x is input
